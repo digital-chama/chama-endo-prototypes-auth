40b042ff7ac81cf41bec4bbe83bf4051758dfb69
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2025-03-31 19:42:07 +0000

    fea(add user authentication utilities and update form state types)

--- src/lib/utils/db/supabase/auth.ts
+++ src/lib/utils/db/supabase/auth.ts
@@ -1,4 +1,31 @@
 import { cookies } from "next/headers";
-import { createClient } from "./server";
 import { cache } from "react";
-import type {Tables} from '@types'
+import type { Tables } from "@/types";
+import { createClient } from "@/lib/utils/db/supabase/server";
+
+export const getUser = cache(async () => {
+  cookies();
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user;
+});
+
+export const getUserDetails = cache(async (userId: string) => {
+  cookies();
+  const supabase = await createClient();
+
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", userId)
+      .single();
+
+    if (error) throw error;
+    return data as Tables<"users">;
+  } catch (error) {
+    console.error("Error in getUserDetails", error);
+  }
+});
--- src/types/auth/index.ts
+++ src/types/auth/index.ts
@@ -1,15 +1,27 @@
+import { AlertDefinition } from "@/lib/auth/alerts/auth-alerts";
 import { BaseFormState } from "../api";
 
-
 export interface AuthFormState extends BaseFormState {
-    errors?: {
-      fullName?: string
-      email?: string
-      password?: string
-      general?: string
-    }
-    verificationPending?: boolean
-    email?: string
-    alert?: AlertDefinition
-  }
-  
+  errors?: {
+    fullName?: string;
+    email?: string;
+    password?: string;
+    general?: string;
+  };
+  verificationPending?: boolean;
+  email?: string;
+  alert?: AlertDefinition;
+}
+export interface ResetPasswordFormState extends BaseFormState {
+  errors?: {
+    email?: string;
+    general?: string;
+  };
+}
+export interface UpdatePasswordFormState extends BaseFormState {
+  errors?: {
+    password?: string;
+    confirmPassword?: string;
+    general?: string;
+  };
+}
